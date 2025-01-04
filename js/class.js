db7a8feadb2d2b87384cedc3009c347fb5d5fa4a
lenrendraw1114 <lenrendraw1114@gmail.com>
2025-01-04 16:32:28 +0900

    16章

---
+++ js/class.js
@@ -0,0 +1,11 @@
+// クラスを定義する
+class Product {
+    // インスタンス化すると同時に処理を実行（初期化）する
+    constructor() {
+      console.log('敏感肌にも優しい100%天然由来のシャンプーです。');
+    }
+  }
+  
+  // インスタンス化する
+  const shampoo = new Product();
+  
